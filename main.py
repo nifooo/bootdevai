0a7bae21a73b6d061015625e5de2ee1628ca7742
nifooo <nifo2013@gmx.de>
2026-01-06 21:28:55 +0100

    feat: add api call

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+from google import genai
+client = genai.Client(api_key=api_key)
+
+
 def main():
     print("Hello from bootdevai!")
 
+response = client.models.generate_content(
+    model='gemini-2.5-flash', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+)
+print(response.text)
 
 if __name__ == "__main__":
     main()
+
+if api_key := None:
+    raise RuntimeError("API Key nicht gefunden")
