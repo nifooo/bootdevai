0ba0d7f6d2e2820db9f59b48e9fd37ffee2c2cf0
nifooo <nifo2013@gmx.de>
2026-01-07 19:27:58 +0100

    feat: added token counter

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def main():
 response = client.models.generate_content(
     model='gemini-2.5-flash', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
 )
+print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+print("Response tokens:", response.usage_metadata.candidates_token_count)
 print(response.text)
 
 if __name__ == "__main__":
