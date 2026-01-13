c3bb5e9c3c36a189591b2809b57f3055882d9876
nifooo <nifo2013@gmx.de>
2026-01-13 18:14:45 +0100

    feat: added tests

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,19 +1,19 @@
 import os
 def get_files_info(working_directory, directory="."):
 
-absolut_path = os.path.abspath(working_directory)
-target_dir = os.path.normpath(os.path.join(absolut_path, directory))
-# Will be True or False
-valid_target_dir = os.path.commonpath([absolut_path, target_dir]) == absolut_path
-	if valid_target_dir = False:
+	absolut_path = os.path.abspath(working_directory)
+	target_dir = os.path.normpath(os.path.join(absolut_path, directory))
+	# Will be True or False
+	valid_target_dir = os.path.commonpath([absolut_path, target_dir]) == absolut_path
+	if valid_target_dir == False:
 		f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-if os.path.isdir(directory) = False:
+	elif os.path.isdir(directory) == False:
 		f'Error: "{directory}" is not a directory'
 
-for item in os.listdir(valid_target_dir):
-	print(item.name)
-	print (os.path.getsize(item))
-	print(os.path.isdir(item))
+	for item in os.listdir(valid_target_dir):
+		print(item.name)
+		print (os.path.getsize(item))
+		print(os.path.isdir(item))
 
 
 
