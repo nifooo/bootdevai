0b4b27d507b31d2acd01e563380154f1918efe5e
nifooo <nifo2013@gmx.de>
2026-01-17 21:02:01 +0100

    fixes: some bug fixes for get file content function

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,3 +1,5 @@
+
+import os
 def get_file_content(working_directory, file_path):
 	try:
 		absolut_path = os.path.abspath(working_directory)
@@ -9,13 +11,14 @@ def get_file_content(working_directory, file_path):
 		elif os.path.isfile(target_dir) == False:
 			return f'Error: File not found or is not a regular file: "{file_path}"'
         
-        MAX_CHARS = 10000
-        with open(file_path, "r") as f:
-            file_content_string = f.read(MAX_CHARS)
+		MAX_CHARS = 10000
+		with open(target_dir, "r") as f:
+			file_content_string = f.read(MAX_CHARS)
 			
             # After reading the first MAX_CHARS...
-            if f.read(1):
-                content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
-	 
+			if f.read(1):
+				file_content_string += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+			return file_content_string
+		
 	except Exception as e:
 	    return f"Error: Error while processing {e}"
