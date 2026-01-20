92fcf48186882cc782976d7bbbf7648c3b78272e
nifooo <nifo2013@gmx.de>
2026-01-20 12:50:27 +0100

    fix: fixes for write file feature

--- functions/write_file.py
+++ functions/write_file.py
@@ -2,22 +2,19 @@ import os
 def write_file(working_directory, file_path, content):
 	try:
 		absolut_path = os.path.abspath(working_directory)
-		target_dir = os.path.normpath(os.path.join(absolut_path, directory))
+		target_dir = os.path.normpath(os.path.join(absolut_path, file_path))
 		# Will be True or False
 		is_valid_target_dir = os.path.commonpath([absolut_path, target_dir]) == absolut_path
 		if is_valid_target_dir == False:
 			return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
 		elif os.path.isdir(target_dir) == True:
 			return f'Error: Cannot write to "{file_path}" as it is a directory'
-		liste = []
-
-		for filename in os.listdir(target_dir):
-			full_path = os.path.join(target_dir, filename)
-			size = os.path.getsize(full_path)
-			is_dir = os.path.isdir(full_path)
-			entry = f"- {filename}: file_size={size} bytes, is_dir={is_dir}"
-			liste.append(entry)
-		return "\n".join(liste)
+		dir_name = os.path.dirname(target_dir)
+		if dir_name:
+			os.makedirs(dir_name, exist_ok=True)
+		with open(target_dir, "w") as f:
+			f.write(content)
+			return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
 	 
 	except Exception as e:
 		return f"Error: Error while processing {e}"
