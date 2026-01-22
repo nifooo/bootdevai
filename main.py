dcd0456d2d02dff722f2178506000ed6a86c82b6
nifooo <nifo2013@gmx.de>
2026-01-22 21:28:09 +0100

    feat: added prompt function

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google.genai import types
+from prompts import system_prompt
+from call_function import available_functions
+
+
 
 
 
@@ -16,7 +20,7 @@ client = genai.Client(api_key=api_key)
 def main():
   
     
-    print("Hello from bootdevai!")
+    print("")
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 parser.add_argument("user_prompt", type=str, help="User prompt")
@@ -24,7 +28,8 @@ args = parser.parse_args()
 # Now we can access `args.user_prompt`
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]  
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents=messages
+    model="gemini-2.5-flash", contents=messages,
+    config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature=0),
 )
 if args.verbose:
     print("User prompt: {user_prompt}")
