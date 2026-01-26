5d7759e54ca3b829e110fee25e627b083e9b4354
nifooo <nifo2013@gmx.de>
2026-01-26 21:30:11 +0100

    feat: making function calls for ai possible part 1

--- call_function.py
+++ call_function.py
@@ -9,3 +9,43 @@ available_functions = types.Tool(
 
 )
 
+def call_function(function_call, verbose=False):
+    if verbose is not None:
+        print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(f" - Calling function: {function_call.name}")
+
+    function_map = {
+    "get_file_content": get_file_content, "get_file_info": get_file_info, "run_python_file": run_python_file, "write_file": write_file   
+}
+
+    function_name = function_call.name or ""
+
+    if function_name not in function_map or "":
+        return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"error": f"Unknown function: {function_name}"},
+            )
+        ],
+    )
+
+    args = dict(function_call.args) if function_call.args else {}
+
+    args["working_directory"] = "./calculator"
+
+    func = function_map[function_name]      # nur holen, nicht ausführen
+    function_result = func(**args)         # hier wird wirklich aufgerufen
+
+    return types.Content(
+    role="tool",
+    parts=[
+        types.Part.from_function_response(
+            name=function_name,
+            response={"result": function_result},
+        )
+    ],
+)
+
